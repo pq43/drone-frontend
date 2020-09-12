bff13df403ef3a5ac0c9343cedfdc23ae49e3919
Nikhil Mehta <shugis2op@gmail.com>
2020-09-12 15:05:45 -0700

    setup for volley testing

--- app/src/main/java/com/example/dronefrontend/MainActivity.java
+++ app/src/main/java/com/example/dronefrontend/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.dronefrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.sendBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("hi");
+            }
+        });
     }
+
 }
