9685d47f45ce26c4685741892da28ea920fd7951
Nikhil Mehta <shugis2op@gmail.com>
2020-09-17 01:00:20 -0700

    volley works U.U

--- app/src/main/java/com/example/dronefrontend/MainActivity.java
+++ app/src/main/java/com/example/dronefrontend/MainActivity.java
@@ -3,21 +3,55 @@ package com.example.dronefrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    Button sendBtn;
+    TextView text;
+    String json_url = "https://jsonplaceholder.typicode.com/todos/1";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.sendBtn);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                System.out.println("hi");
+        sendBtn = (Button)findViewById(R.id.sendBtn);
+        text = (TextView)findViewById(R.id.text);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, json_url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                text.setText(response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        text.setText("didn't work");
+                        error.printStackTrace();
+                    }
+                });
+                MySingleton.getInstance(MainActivity.this).addToRequestQueue(jsonObjectRequest);
             }
         });
-    }
 
+    }
 }
